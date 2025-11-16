e61c9aaea8b37a25c671016af46a62472c109011
pranavhabib <pranavhabib1@gmail.com>
2025-11-15 22:16:38 -0500

    Added User Entity

---
+++ src/main/java/edu/ssw590/summitwealthbank/model/User.java
@@ -0,0 +1,20 @@
+package edu.ssw590.summitwealthbank.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+}
