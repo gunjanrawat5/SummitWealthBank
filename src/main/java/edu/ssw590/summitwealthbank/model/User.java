2d821a6c63a8b57a7c13f4e4072671f413ed96e5
pranavhabib <pranavhabib1@gmail.com>
2025-11-15 23:50:13 -0500

    Removed all manual getters/setters/constructors cause i am using Lombok.

--- src/main/java/edu/ssw590/summitwealthbank/model/User.java
+++ src/main/java/edu/ssw590/summitwealthbank/model/User.java
@@ -1,9 +1,14 @@
 package edu.ssw590.summitwealthbank.model;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.*;
+import lombok.*;
+
 import java.util.List;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "users")
 public class User {
@@ -18,62 +23,9 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    // Optional: ADMIN or USER
     @Column(nullable = false)
-    private String role = "USER";
+    private String role = "USER"; // default to USER
 
     @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Account> accounts;
-
-
-
-    public User() {}
-
-    public User(String username, String password, String role) {
-        this.username = username;
-        this.password = password;
-        this.role = role;
-    }
-
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public List<Account> getAccounts() {
-        return accounts;
-    }
-
-    public void setAccounts(List<Account> accounts) {
-        this.accounts = accounts;
-    }
 }
